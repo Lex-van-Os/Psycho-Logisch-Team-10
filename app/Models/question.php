8843f3037bae0e2d0e1f7a88370026d4425a1e4d
Lex van Os <vanoslex7@gmail.com>
2023-10-30 15:02:35 +0100

    Added template questions + added model definitions

--- app/Models/question.php
+++ app/Models/question.php
@@ -6,4 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class question extends Model
 {
+    protected $fillable = [
+        'title',
+        'type',
+    ];
+    public function question_options()
+    {
+        $this->hasMany(question_option::class);
+    }
 }
---
+++ database/seeders/QuestionTableSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\question;
+
+class QuestionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        question::create([
+            'title' => 'Wat zijn de positieve ervaringen die je hebt gehad met betrekking tot dit onderwerp?',
+            'type' => 'open_question'
+        ]);
+
+        question::create([
+            'title' => 'Wat is de impact geweest van deze ervaring? (Positief)',
+            'type' => 'open_question'
+        ]);
+
+        question::create([
+            'title' => 'Wat zijn de negatieve ervaringen die je hebt gehad met betrekking tot dit onderwerp?',
+            'type' => 'open_question'
+        ]);
+
+        question::create([
+            'title' => 'Wat is de impact geweest van deze ervaring? (Negatief)',
+            'type' => 'open_question'
+        ]);
+
+        question::create([
+            'title' => 'Hoe hebben andere mensen impact gehad op jouw ervaringen?',
+            'type' => 'open_question'
+        ]);
+
+        question::create([
+            'title' => 'Wat is een grote uitdaging waar je tegenaan bent gelopen met betrekking tot dit onderwerp?',
+            'type' => 'open_question'
+        ]);
+
+        question::create([
+            'title' => 'Hoe heb je deze uitdaging overwonnen?',
+            'type' => 'open_question'
+        ]);
+
+        question::create([
+            'title' => 'Wat is een voorbeeld van een gebeurtenis die je overtuigingen met betrekking tot dit onderwerp heeft doen veranderen?',
+            'type' => 'open_question'
+        ]);
+    }
+}
