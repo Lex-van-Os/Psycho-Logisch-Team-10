ca4e50658d4efaf3233c451f824434ea3bcfe728
Lex van Os <vanoslex7@gmail.com>
2023-11-15 13:41:21 +0100

    Verbeterd van aanmaken van demo reflection trajectory met Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ReflectionSeeder::class);
-        $this->call(OpenQuestionSeeder::class);
+        // $this->call(OpenQuestionSeeder::class);
         $this->call(ScaleQuestionSeeder::class);
         $this->call(MultipleChoiceQuestionSeeder::class);
     }
