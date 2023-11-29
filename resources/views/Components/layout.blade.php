2af76fb3061e90e7b5fdf4ff4f702d56b17097a1
ItsNiels99 <43269780+ItsNiels99@users.noreply.github.com>
2023-11-29 14:09:18 +0100

    added style and almost finished css

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,37 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!--Title neerzetten -->
     <title> {{$title ?? 'Psycho-logisch'}}</title>
     @vite('resources/css/app.css')
-    <style>
-        /* width */
-        ::-webkit-scrollbar {
-            width: 10px;
-        }
-
-        /* Track */
-        ::-webkit-scrollbar-track {
-            background: #f1f1f1;
-        }
-
-        /* Handle */
-        ::-webkit-scrollbar-thumb {
-            background: #888;
-        }
-
-        /* Handle on hover */
-        ::-webkit-scrollbar-thumb:hover {
-            background: #555;
-        }
-    </style>
+    @push('css')
+        <link rel="stylesheet" href="/resources/css/layout-style.css">
+    @endpush
 </head>
 <body class="dark:bg-gray-700 bg-gray-100">
-@extends('/Components/navbar')
-@section('layout')
-    <p>Hello!</p>
-@endsection
-<div class="relative h-14 w-full dark:bg-gray-800 bg-gray-100">
-</div>
+@include('Components.navbar')
+@yield('content')
 {{$slot}}
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,25 +2,25 @@
     <x-slot:title>
         Home Page
     </x-slot:title>
+    <div class="modal-container">
     @if(isset($ref_trajs))
         @foreach($ref_trajs as $ref)
             <x-card href="/reflectionTrajectory/{{$ref->id}}" title="{{$ref->title}}"></x-card>
         @endforeach
     @endif
+    </div>
     <button onclick="openModal()">
         <h1 class="focus:outline-none px-4 bg-gray-900 p-3 ml-3 rounded-lg text-white hover:bg-gray-800 text-primary-500  mb-8 text-4xl font-extrabold tracking-tight lg:text-4xlxl dark:text-white text-gray-900">+</h1>
     </button>
     <div class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
          style="background: rgba(0,0,0,.7);">
-        <div
-            class="border border-teal-500 shadow-lg modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+        <div class="border border-teal-500 shadow-lg modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
             <div class="modal-content py-4 text-left px-6">
                 <!--Title-->
                 <div class="flex justify-between items-center pb-3">
                     <p class="text-2xl font-bold">Nieuwe reflectie</p>
                     <div class="modal-close cursor-pointer z-50">
-                        <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                             viewBox="0 0 18 18">
+                        <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
                             <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
                         </svg>
                     </div>
@@ -82,3 +82,4 @@
         }
     }
 </script>
+
