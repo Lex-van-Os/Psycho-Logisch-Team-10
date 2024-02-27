ca339be52ad16a4350bffb555122dd14a19c3461
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2024-02-27 22:23:06 +0100

    vite config fixed, changed welcome

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
     plugins: [
         laravel(['resources/css/app.css',
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-                'resources/js/*.js']),
+            'resources/sass/app.scss',
+            'resources/js/app.js',
+            'resources/js/bootstrap.js',
+            'resources/js/reflectionSummary.js',
+            'resources/js/profile/userProfile.js',
+            'resources/js/shared/toaster.js']),
         tailwindcss('./tailwind.config.js'),
     ],
 });
